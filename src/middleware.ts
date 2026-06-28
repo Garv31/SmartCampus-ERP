336f10e8ff459daabc975bc91a38d8020423ff2c
Garv31 <garv31106@gmail.com>
2026-06-28 14:48:44 +0530

    Reduce middleware bundle size

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
-import { auth } from "@/auth";
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
 import { NextResponse } from "next/server";
 
+const { auth } = NextAuth(authConfig);
+
 export default auth((req) => {
-  const { pathname } = req.nextUrl;
   const user = req.auth?.user;
 
   if (!user) {
